b3336a31111b4bcfb30a4f38c585cf36a224f034
Jonathan <modjow.jm9@gmail.com>
2015-05-19 01:17:04 +0200

    Fb

--- index/php/form.php
+++ index/php/form.php
@@ -1,11 +1,10 @@
 <?php
-    if(isset($_POST['first_name']) && isset($_POST['last_name']) && isset($_POST['email']) && isset($_POST['text'])){
-
-        $first_name = stripslashes(htmlspecialchars($_POST['first_name'])); 
-        $last_name = stripslashes(htmlspecialchars($_POST['last_name'])); 
-        $sender = stripslashes($_POST['email']); 
+    if(/*isset($_POST['first_name']) && isset($_POST['last_name']) && isset($_POST['email']) && */isset($_POST['text'])){
+        //$first_name = stripslashes(htmlspecialchars($_POST['first_name'])); 
+        //$last_name = stripslashes(htmlspecialchars($_POST['last_name'])); 
+        //$sender = stripslashes($_POST['email']); 
         $text = stripslashes(htmlspecialchars($_POST['text']));
-
+        /*
         $regex_mail = '/^[-+.\w]{1,64}@[-.\w]{1,64}\.[-.\w]{2,6}$/i';
         $regex_head = '/[\n\r]/';
 
@@ -20,10 +19,10 @@
         }elseif (preg_match($regex_head, $sender) || preg_match($regex_head, $last_name)){
             exit();
         }else{
-
+        */
             if(strlen($text) > 300) $text = substr($text, 0,300);
-            if(strlen($first_name) > 25) $first_name = substr($first_name, 0,25);
-            if(strlen($last_name) > 25) $last_name = substr($last_name, 0,25);
+            //if(strlen($first_name) > 25) $first_name = substr($first_name, 0,25);
+            //if(strlen($last_name) > 25) $last_name = substr($last_name, 0,25);
 
             $counter_name = "index_message.txt";
             // Check if a text file exists. If not create one and initialize it to zero.
@@ -43,10 +42,10 @@
                 $_SESSION['hasVisited']="yes";
                 $json['count'] = $json['count'] + 1;
 
-                $json[$json['count']]['date'] = date("Y-m-d H:i:s");
-                $json[$json['count']]['first_name'] = $first_name;
-                $json[$json['count']]['last_name'] = $last_name;
-                $json[$json['count']]['sender'] = $sender;
+                //$json[$json['count']]['date'] = date("Y-m-d H:i:s");
+                //$json[$json['count']]['first_name'] = $first_name;
+                //$json[$json['count']]['last_name'] = $last_name;
+                //$json[$json['count']]['sender'] = $sender;
                 $json[$json['count']]['text'] = $text;
 
                 $f = fopen($counter_name, "w");
@@ -54,7 +53,7 @@
                 fclose($f);
             }
 
-            exit();
-        }
+            echo file_get_contents("http://mercandalli.com");
+        //}
     }
 ?>
