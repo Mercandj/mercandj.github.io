56f5949bdbf4d4e2dca12e4190046abaa241b471
Jonathan <modjow.jm9@gmail.com>
2015-07-29 01:15:03 +0200

    Removed old projects

--- index/js/index.js
+++ index/js/index.js
@@ -4,3 +4,14 @@ $(document).ready(function(){
 $(document).ready(function(){
   $('.materialboxed').materialbox();
 });
+
+/*
+$(document).ready(function() {
+    $('#iframe').attr("src", "http://mercandalli.com/FileSpace-Angular").on('load', function() {
+    	//Hack chrome auto scroll
+    	$('body').scrollTop(0);
+        $(this).show();
+        setTimeout(function(){ $('body').scrollTop(0); }, 180);        
+    });
+});
+*/
